bb6a863c31c946340990ff456ef01fd992c3a713
jur3l <csjurel@gmail.com>
2024-06-21 16:45:42 +0200

    Update app.js

--- teszt/js/app.js
+++ teszt/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
   "particles": {
     "number": {
-      "value": 100,
+      "value": 2000,
       "density": {
         "enable": true,
         "value_area": 800
