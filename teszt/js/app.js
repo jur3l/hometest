1691b4432f5d9a2ed18f486793c87a6238a44b7a
jur3l <csjurel@gmail.com>
2024-06-21 16:44:42 +0200

    Update app.js

--- teszt/js/app.js
+++ teszt/js/app.js
@@ -87,7 +87,7 @@ particlesJS('particles-js',
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "grab"
+        "mode": "repulse"
       },
       "onclick": {
         "enable": true,
