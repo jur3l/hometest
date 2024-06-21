1ae27ea92f76935bfc1b7041aec6ac861cba4bc6
jur3l <csjurel@gmail.com>
2024-06-21 16:53:54 +0200

    Update app.js

--- teszt/js/app.js
+++ teszt/js/app.js
@@ -70,7 +70,7 @@ particlesJS('particles-js',
     "move": {
       "enable": true,
       "speed": 6,
-      "direction": "none",
+      "direction": "top-left",
       "random": false,
       "straight": false,
       "out_mode": "out",
